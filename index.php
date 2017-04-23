6571fa0fcdd7d9c523023c9a2e623b95a21b079c
sand <sand_6678@hotmail.com>
2017-04-23 17:17:15 +0700

    add indes.php

--- index.php
+++ index.php
@@ -19,8 +19,22 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text 
+
+					// "text"
 			];
+
+
+			if($text == "HI"){
+                            
+				$messages = [
+				'type' => 'text',
+				'text' => "hello"
+			];
+
+
+
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
