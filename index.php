a3505382c0279822d327a76de67d0ce737037d3e
y2k6678 <sand_6678@hotmail.com>
2017-05-11 05:20:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,13 +30,7 @@ if (!is_null($events['events'])) {
 				$messages = [
 				'type' => 'text',
 				'text' => "hello"
-			];
-			if($text == "Help"){
-                            
-				$messages = [
-				'type' => 'text',
-				'text' => "sand"
-			];
+			] 		
 
 
 
