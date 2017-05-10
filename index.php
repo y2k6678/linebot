002e0a3b4bcb73ec0f5f93c75d617a77c0f82c6d
y2k6678 <sand_6678@hotmail.com>
2017-05-11 06:52:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,18 +30,12 @@ if (!is_null($events['events'])) {
 				$messages = [
 				'type' => 'text',
 				'text' => "hello"
-			]
-			if($text == "K"){
-                            
-				$messages = [
-				'type' => 'text',
-				'text' => "WER"
-			];
-			
-			
+			];		
 
 
 			}
+			
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
