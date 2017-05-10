87c7b24e6351c68c10d187d00422cb5893322e4c
y2k6678 <sand_6678@hotmail.com>
2017-05-11 05:24:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,13 @@ if (!is_null($events['events'])) {
 				$messages = [
 				'type' => 'text',
 				'text' => "hello"
-			]; 		
+			]; 	
+			if($text == "K"){
+                            
+				$messages = [
+				'type' => 'text',
+				'text' => "we"
+			]; 
 
 
 
