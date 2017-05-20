b2612d8d8579bfea88372c29c529f6e1c379a114
y2k6678 <sand_6678@hotmail.com>
2017-05-20 16:55:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 $temp = file_get_contents('https://api.thingspeak.com/channels/262354/fields/1/last.txt');
-$convert = (string) $temp
+
 //convert
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 				
 				$messages = [
 				'type' => 'text',
-				'text' => $convert
+				'text' => $temp
   	
 			];
 			}	
