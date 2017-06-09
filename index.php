8a5359811d7609a19d85425fd71229cb5cc3ebe4
y2k6678 <sand_6678@hotmail.com>
2017-06-09 21:34:12 +0700

    Create index.php

--- index.php
+++ index.php
@@ -37,14 +37,14 @@ if (!is_null($events['events'])) {
 				if($text == "1"){		
 					$messages = [
 					'type' => 'text',
-					'text' => $Light <br>."พิมพ์ help เพื่อดูเมนู"
+					'text' => $Light."พิมพ์ help เพื่อดูเมนู"
 				];	
 			}
 			
 			if($text == "2"){
 				$messages = [ 
 					'type' => 'text',
-					'text' => "#วัดพระธาตุ                   "." #สถานที่อื่นๆ                           "." พิมพ์หมายเลข help เพื่อดูเมนู "                          "
+					'text' => "#วัดพระธาตุ                   "." #สถานที่อื่นๆ                           "."พิมพ์หมายเลข help เพื่อดูเมนู                           "
 						];
 					}
 			if($text == "HI"){
