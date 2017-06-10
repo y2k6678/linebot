2ae31cf0bc8ef8e1efe58f0ed1741f4d8b1a5e82
y2k6678 <sand_6678@hotmail.com>
2017-06-10 15:22:37 +0700

    Create index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (!is_null($events['events'])) {
 				if($text == "1"){		
 					$messages = [
 					'type' => 'text',
-					'text' => echo $Light."\n พิมพ์ help เพื่อดูเมนู"
+					'text' => $Light."\n พิมพ์ help เพื่อดูเมนู"
 				];	
 			}
 			
