ef309b6e4fcaa6828251d3d269fd1673b3af20d7
y2k6678 <sand_6678@hotmail.com>
2017-06-17 22:12:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $HUM = file_get_contents('https://api.thingspeak.com/channels/262354/fields/2/la
 $TEM = file_get_contents('https://api.thingspeak.com/channels/262354/fields/3/last.txt');
 //convert
 
-$text = strtoupper($text);
+
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
@@ -37,6 +37,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => "พิมพ์หมายเลข 1 เพื่อดูแสง"."พิมพ์หมายเลข 2 ดูสถานที่ทั้งหมด"
 			];
+				$text = mb_strtoupper($text);
 			}	
 				if($text == "1"){		
 					$messages = [
