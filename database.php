ebf55f9b8093df898164c8535469827def8206b7
y2k6678 <sand_6678@hotmail.com>
2017-09-16 23:43:49 +0700

    sa

--- database.php
+++ database.php
@@ -1,12 +1,23 @@
 <?php 
+echo $_GET['getid'];
+echo $_GET['getLINK'];
+echo $_GET['getDATETIME'];
+
 $host = "ec2-107-22-211-182.compute-1.amazonaws.com";
 $user = "mmdkvvqziulstc";
 $pass = "e10240d71df70c411f5201bc37491e9091491ff276b8d8b66f8e507ea5b7dc22";
 $db   = "dcv361109jo6fh";
 
+
 $dbconn = pg_connect("host=".$GLOBALS['host']." port=5432 dbname=".$GLOBALS['db']." user=".$GLOBALS['user']." password=".$GLOBALS['pass'])
     or die('Could not connect: ' . pg_last_error());
-   
-echo $dbconn;
-echo "hello wo";
+    pg_connect();
+    $sql = "INSERT INTO weatherstation (ID, LINK, DATETIME)
+   VALUES ('".$_GET['getid']."', '".$_GET['getLINK']."', '".$_GET['getDATETIME']."')";
+    $result = pg_exec($dbconn,$sql);
+
+    pg_close($dbconn);
+
+
+
 ?>
