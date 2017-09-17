b45f6ebb6e66452fdaacf1693ab6c3e2b1f095fc
y2k6678 <sand_6678@hotmail.com>
2017-09-17 23:26:02 +0700

    ghjskj

--- test.php
+++ test.php
@@ -2,10 +2,15 @@
 echo "Today is " . date("Y-m-d") . "<br>";
 
 $date=date("Y-m-d");
-
-for($i=1;$i<13;$i++)
-{
-    echo "$date $i";
+function showtime($time){
+    $h =split(":",$time);
+    echo $h[0] . "   ".$h[1];
 }
+
+showtime("24:55");
+// for($i=1;$i<13;$i++)
+// {
+//     echo "$date $i";
+// }
 
 ?>
