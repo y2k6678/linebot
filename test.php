474438ecb6b0665033ac0f38aba4d46ffcf55373
y2k6678 <sand_6678@hotmail.com>
2017-09-17 23:10:52 +0700

    gguhf

--- test.php
+++ test.php
@@ -3,13 +3,9 @@ echo "Today is " . date("Y-m-d") . "<br>";
 
 $date=date("Y-m-d");
 
-echo "$date  $i";
-
-// for ($i=1; $i <13 ; $i++) { 
-//     # code...
-//     $date=date("Y-m-d");
-//     echo "$date  $i";
-// }
-
+for($i=1;$i<13;$i++)
+{
+    echo "$date $i";
+}
 
 ?>
