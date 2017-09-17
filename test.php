578c486a9d225dc34de5b2fb1ffe5a162dbb31f0
y2k6678 <sand_6678@hotmail.com>
2017-09-17 23:03:57 +0700

    fgjjj

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php 
 echo "Today is " . date("Y-m-d") . "<br>";
 
-for ($i=1; $i <13 ; $i++) { 
-    # code...
-    $date=date("Y-m-d");
-    echo "$date  $i";
-}
+// for ($i=1; $i <13 ; $i++) { 
+//     # code...
+//     $date=date("Y-m-d");
+//     echo "$date  $i";
+// }
 
 
 ?>
