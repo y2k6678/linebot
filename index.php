ddaf1143b52212aa8224afa2d812e2cc406d1127
y2k6678 <sand_6678@hotmail.com>
2017-09-23 11:21:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -192,8 +192,8 @@ if (!is_null($events['events']))
 			}
 			if ($text == "qw")
 			{
-				$messages = ['type' => 'location','title'=> 'my location','address': '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
-				'latitude': 35.65910807942215,'longitude': 139.70372892916203];
+				$messages = ['type' => 'location','title'=> 'my location','address'=> '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+				'latitude'=> 35.65910807942215,'longitude'=> 139.70372892916203];
 			}
 
 
