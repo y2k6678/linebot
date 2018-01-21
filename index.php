8743460f01a21ce11377748418ad3fb5595b2777
y2k6678 <sand_6678@hotmail.com>
2018-01-21 21:25:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -328,7 +328,7 @@ if (!is_null($events['events']))
 				}
 				$messages = ['type' => 'image', 'originalContentUrl' => $templink, 'previewImageUrl' => $templink];
 			}
-$text=trim($text,' ');
+$text=trim($text);
 			$textSplited = split(" ", $text);
 			if ( ereg_replace('[[:space:]]+', '', trim($textSplited[0])) == "ภาพ")
 			{
@@ -344,12 +344,15 @@ $text=trim($text,' ');
 
 				//$messages = ['type' => 'text', 'text' => "HI $dataFromshowtime[0] \n$dataFromshowtime[1] \n$templink"
 				if ($qcount > 0){
-				$messages = [
-				'type' => 'image',
-				'originalContentUrl' => $templink,
-					'previewImageUrl' => $templink
+				// $messages = [
+				// 'type' => 'image',
+				// 'originalContentUrl' => $templink,
+				// 	'previewImageUrl' => $templink
 
-				];}
+				// ];
+				$messages = ['type' => 'text', 'text' => "$text"];
+				
+			}
 				else {
 					$messages = [
 						'type' => 'image',
