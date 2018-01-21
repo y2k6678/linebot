54445559ea2b385c970e4d285d66aa9e4f824a47
y2k6678 <sand_6678@hotmail.com>
2018-01-21 21:58:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -328,7 +328,7 @@ if (!is_null($events['events']))
 				}
 				$messages = ['type' => 'image', 'originalContentUrl' => $templink, 'previewImageUrl' => $templink];
 			}
-			$ws= (split(" ", $temptext)).count();
+			$ws= count(split(" ", $temptext));
 $temptext = trim($text,' ');
 			$textSplited = split(" ", $temptext);
 			if ( ereg_replace('[[:space:]]+', '', trim($textSplited[0])) == "ภาพ")
